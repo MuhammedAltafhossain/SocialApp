5ec371b1c1bf4c4fd85d5ff3a04e50302c6dc7f5
shamir <shamirroy2020@gmail.com>
2023-06-17 20:13:49 +0600

    SAV Sprint 1 - Commit and Push On boarding Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'screens/introScreen.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -11,12 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Social App'),
+      home: IntroPage(),
     );
   }
 }
